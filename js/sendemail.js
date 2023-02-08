e8765841831ca367bc693a34b84addf629c0777c
McMelonTV <62901799+McMelonTV@users.noreply.github.com>
2023-02-08 15:32:32 +0100

    email should be redone

--- js/sendemail.js
+++ js/sendemail.js
@@ -1,15 +1,6 @@
 function sendEmail() {
-    var email = document.getElementById("email").value;
     var subject = document.getElementById("name").value;
     var message = document.getElementById("message").value;
 
-    Email.send({
-        Host : "smtp.mail.yahoo.com",
-        Username : "sena.ink@yahoo.com",
-        Password : "blu3.f1sh",
-        To : "d4vss@outlook.com",
-        From : "sena.ink@yahoo.com",
-        Subject : "New message from " + subject,
-        Body : email + "\n\n" + message
-    })
+    window.location.href(`mailto:sena4k4.ink@gmail.com?subject=${subject}&body=${message}`)
 }
